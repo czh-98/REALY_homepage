d6ec20372cb51c1b4586638a781140b257bc1b1a
fangzy97 <1299012718@qq.com>
2023-01-02 15:58:21 +0800

    Add font awaresome

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,18 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faFilePdf } from "@fortawesome/free-regular-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
+
 const app = createApp(App);
 
+library.add(faFilePdf, faGithub, faPaperclip);
+
 app.use(ElementPlus);
+app.component("FontAwesomeIcon", FontAwesomeIcon);
 app.mount("#app");
